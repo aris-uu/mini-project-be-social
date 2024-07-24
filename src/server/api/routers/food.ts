591283795c8e6a669305068a1735482b211aecfb
arisu <arisuuarisuu@gmail.com>
2024-07-24 20:39:23 +0700

    feat: add API returns and error handling

--- src/server/api/routers/food.ts
+++ src/server/api/routers/food.ts
@@ -3,14 +3,15 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 import { food } from "~/server/db/schema";
 import { eq } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
 
 export const foodRouter = createTRPCRouter({
     addFood: publicProcedure
         .input(z.object({ name: z.string() }))
         .mutation(async ({ ctx, input }) => {
-            await ctx.db.insert(food).values({
+            return await ctx.db.insert(food).values({
                 name: input.name
-            });
+            }).returning();
         }),
     getAllFood: publicProcedure.query(({ ctx }) => {
         return ctx.db.query.food.findMany();
@@ -18,13 +19,28 @@ export const foodRouter = createTRPCRouter({
     updateFood: publicProcedure
         .input(z.object({ id: z.string(), name: z.string() }))
         .mutation(async ({ ctx, input }) => {
-            await ctx.db.update(food).set({
+            const result = await ctx.db.update(food).set({
                 name: input.name
-            }).where(eq(food.id, input.id))
+            }).where(eq(food.id, input.id)).returning();
+
+            if (result.length === 0) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Food not found"
+                });
+            }
         }),
     deleteFood: publicProcedure
-        .input(z.object({id:z.string()}))
-        .mutation(async ({ctx, input}) => {
-            await ctx.db.delete(food).where(eq(food.id, input.id))
+        .input(z.object({ id: z.string() }))
+        .mutation(async ({ ctx, input }) => {
+            const result = await ctx.db.delete(food).where(eq(food.id, input.id))
+                .returning();
+
+            if (result.length === 0) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Food not found"
+                });
+            }
         })
 });
